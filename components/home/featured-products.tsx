28d61e5d2cdad9a5d2c244a344edfaf45df66aac
Andres Granada <andresgranada0878@gmail.com>
2026-08-22 08:20:00 -0500

    fix: improve home featured products mobile layout

--- components/home/featured-products.tsx
+++ components/home/featured-products.tsx
@@ -36,7 +36,7 @@ export function FeaturedProducts({ products }: { products: Product[] }) {
         </Button>
       </div>
 
-      <div className="mt-6 grid grid-cols-2 gap-3 sm:grid-cols-3 md:gap-4 lg:grid-cols-4">
+      <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4">
         {products.map((product, index) => (
           <ProductCard
             key={product.slug}
@@ -48,4 +48,5 @@ export function FeaturedProducts({ products }: { products: Product[] }) {
     </section>
   )
 }
+
 
